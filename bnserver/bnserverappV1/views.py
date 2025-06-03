30c0ac1f52fa2a40cefffbabb7b0e8e85780f0f9
DeniBademi <denis.zaharievv@gmail.com>
2025-06-03 12:08:22 +0300

    changes

--- bnserver/bnserverappV1/views.py
+++ bnserver/bnserverappV1/views.py
@@ -118,7 +118,7 @@ def get_metadata(request, network_id):
 @require_auth
 def predict(request):
     if request.method != 'POST':
-        return HttpResponse("Only POST method is allowed")
+        return HttpResponse(f"Only POST method is allowed, got {request.method}")
     try:
         json_data = json.loads(request.body)
         print(json_data)
